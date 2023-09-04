090f2d553a83ec320e6b34cd7aa89a41783869b1
kxamidjonov <k.xamidjonov@uzinfocom.uz>
2023-09-04 15:42:19 +0500

    csrf config

--- config/settings.py
+++ config/settings.py
@@ -143,4 +143,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['softdev.uz', 'www.softdev.uz']
+CSRF_TRUSTED_ORIGINS = ['https://softdev.uz', 'https://www.softdev.uz']
